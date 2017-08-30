dc19c3d5ba590411149a3c41105dd39bbdd53908
binknam <twitcht.maxvolt@gmail.com>
2017-08-31 00:56:02 +0700

    finish part 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,22 @@ public class PlayerController : MonoBehaviour {
 
 	public float tilt;
 	public float speed;
+	public Object shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
 	private float Xmin = -6.8f, Xmax = 6.8f, Zmin = -4, Zmax = 8;
+	private float nextFire;
+
+	void Update() {
+
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
+
+	}
 
 	void FixedUpdate()
 	{
@@ -24,6 +39,6 @@ public class PlayerController : MonoBehaviour {
 				Mathf.Clamp (GetComponent<Rigidbody> ().position.z, Zmin, Zmax)
 		);
 
-		GetComponent<Rigidbody> ().rotation = new Vector3 (0.0f, 0.0f, GetComponent<Rigidbody> ().velocity.x * tilt);
+		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody> ().velocity.x * tilt);
 	}
 }
